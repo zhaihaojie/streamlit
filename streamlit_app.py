052159ee4d4b13ef4fe062740ada27c9ff9647a5
zhaihaojie <852865156@qq.com>
2023-08-27 15:33:15 +0800

    测试模块

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,5 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
-
-"""
-数据可视化
-"""
-
 from search_data_explorer import SearchDataExplorer
 
 sde = SearchDataExplorer()
+print(sde)
 sde.open()
